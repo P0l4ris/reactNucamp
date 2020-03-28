442ea11faa61772f8283ab914513d748264dd0d8
Luis Juarros <juarrosl0806@gmail.com>
2020-03-28 08:56:48 -0700

    Breadcrumbs

--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDirectoryItem({campsite}) {
     return (
         <Card>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            {/* here we made a dynamic link for each object, giving it its directory path */}
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 
@@ -28,6 +32,16 @@ function Directory(props) {
         //this is where we send data back to the parent component. top level return
         return (
             <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Directory</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>Directory</h2>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                     {directory} 
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,11 @@ import Header from './HeaderComponents';
 import Footer from './FooterComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 //imported react router dom. brains of our router
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -12,27 +16,48 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 //now we have local state in App not the child Directory
 
 //directory is now presentational component
+
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
                 campsites: CAMPSITES,
+                comments: COMMENTS,
+                partners: PARTNERS,
+                promotions: PROMOTIONS,
         };
     }
 
   render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home           
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                 />
             );
         }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+                />
+            );
+        };
+
+
       return (
           <div>
               <Header />
               <Switch>
                   <Route path='/home' component={HomePage} />
-                  {/* here we use the boolean exact and routes are like case in switch*/}
                   <Route exact path ='/directory' render={() => <Directory campsites={this.state.campsites} /> } />
+                  {/* this colon stores what comes after directory as a parameter in an object called campsiteId..it's like a tracker. part of the route components state "match". Match's object gets passed as a prop for the object CampsiteWithId line:40*/}
+                  <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                  <Route exact path='/contactus' component={Contact} />
                   <Route to='/home' />
               </Switch>
               <Footer />
