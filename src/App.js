53e42da2935e54f4682f106ace4a02a88321a258
Luis Juarros <juarrosl0806@gmail.com>
2020-04-01 23:41:46 -0700

    Intro to Redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, {Component}  from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import './App.css';
+
+//callback the redux store
+const store = ConfigureStore();
 
 //now we have local state in App not the child Directory
 class App extends Component {
 
   render() {
       return (
+        //here <Provider> returns the redux sore to all children of App
+        <Provider store={store}>
         <BrowserRouter>
             <div className="App">
                 <Main />  
             </div>
           </BrowserRouter>
+        </Provider>  
       );
   }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,37 +5,42 @@ import Footer from './FooterComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
 import About from '../shared/AboutComponent';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+//transferring state to the redux store; no longer here
+import { connect } from 'react-redux';
+// import { CAMPSITES } from '../shared/campsites';
+// import { COMMENTS } from '../shared/comments';
+// import { PARTNERS } from '../shared/partners';
+// import { PROMOTIONS } from '../shared/promotions';
+
+
 //imported react router dom. brains of our router
-import { Switch, Route, Redirect } from 'react-router-dom';
 
 
-//now we have local state in App not the child Directory
 
-//directory is now presentational component
+//main component is a container and all these objects rendered here are children presentational component
+
+//in here, we pass state from redux as props
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments,
+        partners: state.partners,
+        promotions: state.promotions
+    };
+};
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-                campsites: CAMPSITES,
-                comments: COMMENTS,
-                partners: PARTNERS,
-                promotions: PROMOTIONS,
-        };
-    }
+  
 
   render() {
         const HomePage = () => {
             return (
                 <Home           
-                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                    campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.props.partners.filter(partner => partner.featured)[0]}
                  />
             );
         }
@@ -43,8 +48,8 @@ class Main extends Component {
         const CampsiteWithId = ({match}) => {
             return (
                 <CampsiteInfo 
-                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
                 />
             );
         };
@@ -55,11 +60,11 @@ class Main extends Component {
               <Header />
               <Switch>
                   <Route path='/home' component={HomePage} />
-                  <Route exact path ='/directory' render={() => <Directory campsites={this.state.campsites} /> } />
+                  <Route exact path ='/directory' render={() => <Directory campsites={this.props.campsites} /> } />
                   {/* this colon stores what comes after directory as a parameter in an object called campsiteId..it's like a tracker. part of the route components state "match". Match's object gets passed as a prop for the object CampsiteWithId line:40*/}
                   <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                   <Route exact path='/contactus' component={Contact} />
-                  <Route exact path='/aboutus' render ={() => <About partners={this.state.partners} /> } />
+                  <Route exact path='/aboutus' render ={() => <About partners={this.props.partners} /> } />
                   <Route to='/home' />
               </Switch>
               <Footer />
@@ -68,4 +73,5 @@ class Main extends Component {
   }
 }
 
-export default Main;
+//react router to work with changes to the export?
+export default withRouter(connect(mapStateToProps)(Main));
